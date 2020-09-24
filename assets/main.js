e36d9d4185847609925cab8b0aeca63af199b65f
Ademola Kolawole <kademola@Wales-MBP.home>
2020-09-24 17:33:14 -0400

    scripting done

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,31 @@
 // COMPLETE THE TODOs BELOW...
-// Note: The primary TODOs for this Activity will not require 
+// Note: The primary TODOs for this Activity will not require
 // you to edit the HTML or CSS.
 
 // On our HTML page, we have a #search-button, a #textbox input,
 // box a #sentence, and a #search-results DIV.
 
-// TODO 1: Use querySelector to assign EACH of the above 
+// TODO 1: Use querySelector to assign EACH of the above
 // elements to a variable, just like this:
-let searchButton = document.querySelector('#search-button')
+let searchButton = document.querySelector("#search-button");
+let textBox = document.querySelector("#textbox");
+let sentence = document.querySelector("#sentence");
+let searchResults = document.querySelector("#search-results");
 
-searchButton.addEventListener('click', function () {
- 
+searchButton.addEventListener("click", function () {
   // TODO 2: Write an IF statement which determines whether
   // the user-supplied string from #textbox is included in
   // the #sentence string.
-  
-  // Hint 1: To get the user-supplied string from the input 
+  let textValue = textBox.value.toLowerCase().trim();
+  let sentText = sentence.innerText.toLowerCase().trim();
+
+  if (sentText.includes(textValue)) {
+    searchResults.innerText = "A match was found";
+  } else {
+    searchResults.innerText = "No Match. Too bad!";
+  }
+
+  // Hint 1: To get the user-supplied string from the input
   // box (#textbox), use the property .value on the variable
   // you assigned the textbox element to.
 
@@ -25,15 +35,14 @@ searchButton.addEventListener('click', function () {
 
   // Hint 3: You can check whether string1 contains string2
   // by using string1.includes(string2) – which will return
-  // true or false. 
+  // true or false.
 
   // TODO 3: If the user-supplied string is included in the
   // #sentence string, update the innerText of #search-results
   // with a success message (such as, "A match was found!"),
-  // otherwise update it with a failure message (such as, 
+  // otherwise update it with a failure message (such as,
   // "No results. Too bad!")
-
-})
+});
 
 // STRETCH GOALS (easiest to hardest):
 //
@@ -41,9 +50,9 @@ searchButton.addEventListener('click', function () {
 //  • Use .trim() to remove unnecessary whitespace from the
 //    user-supplied string.
 //  • Use .toLowerCase() on both the sentence and the user-
-//    supplied string, so that your word search is 
+//    supplied string, so that your word search is
 //    NOT case-sensitive.
 //  • Count the number of times the word appears.
 //  • Display the word count for the user.
-//  • Find a way to highlight the matching word(s). 
+//  • Find a way to highlight the matching word(s).
 //    Hint: .split() and Iteration.
